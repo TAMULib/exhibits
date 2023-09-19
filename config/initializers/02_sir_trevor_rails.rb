f61816d04627af93d0d15749136d576429c6ba10
qtamu <qtamu@297558SL-L5520.library.tamu.edu>
2023-09-19 16:02:31 -0500

    move sir trevor rails initializer higher up to become initialized sooner

--- config/initializers/02_sir_trevor_rails.rb
+++ config/initializers/02_sir_trevor_rails.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-# Required for 0.6 and up:
-# https://github.com/madebymany/sir-trevor-rails#upgrade-guide-to-v060
-class SirTrevorRails::Block
-  def self.custom_block_types
-    # You can define your custom block types directly here or in your engine config.
-    Spotlight::Engine.config.sir_trevor_widgets
+require 'stir_trevor_rails'
+
+Rails.application.config.after_initialize do
+  # Required for 0.6 and up:
+  # https://github.com/madebymany/sir-trevor-rails#upgrade-guide-to-v060
+  class SirTrevorRails::Block
+    def self.custom_block_types
+      # You can define your custom block types directly here or in your engine config.
+      Spotlight::Engine.config.sir_trevor_widgets
+    end
   end
 end
