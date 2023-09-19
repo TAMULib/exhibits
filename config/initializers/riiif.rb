6cce0317886abd9d408edda81f297ea073f75502
qtamu <qtamu@297558SL-L5520.library.tamu.edu>
2023-09-19 15:05:25 -0500

    updated riiif file to run after initialization

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
+require 'riiif'
 
-# Riiif::Image.authorization_service = IIIFAuthorizationService
+Rails.application.config.after_initialize do
+    Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
 
-# Riiif.not_found_image = 'app/assets/images/us_404.svg'
-#
-Riiif::Engine.config.cache_duration = 365.days
+    # Riiif::Image.authorization_service = IIIFAuthorizationService
+
+    # Riiif.not_found_image = 'app/assets/images/us_404.svg'
+    #
+    Riiif::Engine.config.cache_duration = 365.days
+end
