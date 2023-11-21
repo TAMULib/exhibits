0c77905d32dccfe85a9e20e9d26baee6b1515b40
qtamu <qtamu@297558SL-L5520.library.tamu.edu>
2023-11-21 13:55:24 -0600

    set the css compressor to nil to fix error

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TamuSpotlight
     config.action_mailer.default_url_options = { host: "localhost:4200", from: "noreply@example.com" }
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.css_compressor = nil
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
