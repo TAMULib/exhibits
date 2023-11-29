862e08934e980841a098705f5c22a29a480cefd3
qtamu <qtamu@297558SL-L5520.library.tamu.edu>
2023-11-29 11:59:12 -0600

    added from_hash method to sir trevor rails

--- config/initializers/sir_trevor_rails.rb
+++ config/initializers/sir_trevor_rails.rb
@@ -7,4 +7,11 @@ class SirTrevorRails::Block
     # You can define your custom block types directly here or in your engine config.
     Spotlight::Engine.config.sir_trevor_widgets
   end
+  
+  def self.from_hash(hash, parent = nil)
+    hash = hash.deep_dup.with_indifferent_access
+    type_klass(hash).new(hash, parent)
+  end
+
+
 end
