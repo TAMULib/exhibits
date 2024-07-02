8176cefce825cc28b735d933e8972728805d069c
William Welling <wwelling@tamu.edu>
2024-07-02 15:40:35 -0500

    Update search_builder.rb

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
-
-  include Spotlight::AccessControlsEnforcementSearchBuilder
+  include Spotlight::SearchBuilder
 
   ##
   # @example Adding a new step to the processor chain
