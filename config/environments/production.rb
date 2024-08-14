87b19aa4ef065373dfe6fb35379548ba5aef7c20
William Welling <wwelling@tamu.edu>
2024-08-14 10:48:19 -0500

    Disable source maps for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
+  config.assets.debug = false
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
