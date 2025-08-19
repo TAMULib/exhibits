dffb525a58ebb909edcc63af3847c55d52edd93d
qtamu <qsean@tamu.edu>
2025-08-19 13:35:06 -0500

    Rquires riiif

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 unless ENV['SKIP_RIIIF_INIT'] == 'true'
+  require 'riiif'
   Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
 
   # Riiif::Image.authorization_service = IIIFAuthorizationService
